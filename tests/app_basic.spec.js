4632c3a75cbf4839f86862186c68661e64043310
Natalia Śmieja <n.smieja@sportradar.com>
2024-12-09 16:08:31 +0100

    filepath depended on server

--- tests/app_basic.spec.js
+++ tests/app_basic.spec.js
@@ -2,16 +2,21 @@
 const { test, expect } = require('@playwright/test');
 const path = require('path');
 
-const FILEPATH = path.join(__dirname, '../app/index.html');
 const CITIES = ['London', 'Wrocław', 'Dubai', 'Vancouver'];
 const INCORRECT_CITY = 'Abcd';
+let filepath;
 
+if (__dirname.includes('academy')) {
+  filepath = path.join(__dirname, '../app/index.html');
+} else {
+  filepath = path.join(__dirname, './app/index.html');
+}
 
 
 test.describe('', () => {
   test('SCENARIO: User is able to navigate to the main page', async ({ page }) => {
     await test.step('STEP 1: Navigate to the file URL', async () => {
-      await page.goto(FILEPATH);
+      await page.goto(filepath);
       await page.waitForLoadState('load');
     });
 
@@ -30,7 +35,7 @@ test.describe('', () => {
 
   test('SCENARIO: User is able to see the bets after entering a city name and clicking on submit button', async ({ page }) => {
     await test.step('GIVEN: User has navigated to the main page', async () => {
-      await page.goto(FILEPATH);
+      await page.goto(filepath);
       await page.waitForLoadState('load');
     });
 
@@ -50,7 +55,7 @@ test.describe('', () => {
 
   test('SCENARIO: User is able to see the bets after entering a city name and clicking enter', async ({ page }) => {
     await test.step('GIVEN: User has navigated to the main page', async () => {
-      await page.goto(FILEPATH);
+      await page.goto(filepath);
       await page.waitForLoadState('load');
     });
 
@@ -70,7 +75,7 @@ test.describe('', () => {
 
   test('SCENARIO: User is able to see the bets after entering a city name - case insensitive', async ({ page }) => {
     await test.step('GIVEN: User has navigated to the main page', async () => {
-      await page.goto(FILEPATH);
+      await page.goto(filepath);
       await page.waitForLoadState('load');
     });
 
@@ -90,7 +95,7 @@ test.describe('', () => {
 
   test('SCENARIO: User is not able to see the bets after entering incorrect city name', async ({ page }) => {
     await test.step('GIVEN: User has navigated to the main page', async () => {
-      await page.goto(FILEPATH);
+      await page.goto(filepath);
       await page.waitForLoadState('load');
     });
 
@@ -112,7 +117,7 @@ test.describe('', () => {
 
   test('SCENARIO: User is able to add $50 to the wallet by clicking on Top Up button', async ({ page }) => {
     await test.step('GIVEN: User has navigated to the main page', async () => {
-      await page.goto(FILEPATH);
+      await page.goto(filepath);
       await page.waitForLoadState('load');
     });
 
